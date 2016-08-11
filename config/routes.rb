3906fa90a86596b04c36c6fee8c701f98f005524
Loan Robert <loan.rbrt@gmail.com>
2016-08-11 11:28:56 +0200

    models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'doses/create'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
