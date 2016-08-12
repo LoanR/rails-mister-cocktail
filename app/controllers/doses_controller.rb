fdee438125bce585c5c9df717df63fea3c16fc84
Loan Robert <loan.rbrt@gmail.com>
2016-08-12 11:42:36 +0200

    add figaro to protect my bad secrets

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,23 +4,25 @@ class DosesController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path
+      redirect_to @cocktail
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.delete
-    redirect_to cocktail_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
